6b65005ee56de54dc4a1b4ac10ecde35af8c8448
ADHITHYAN K R <theadhithyankr@gmail.com>
2026-02-16 12:50:39 +0530

    feat: Add Firebase integration and Netlify deployment configuration.

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -6,13 +6,13 @@ import { getAnalytics } from 'firebase/analytics';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCREt1hA4eZA02H87kKWO3tdMHxf46sQ50",
-    authDomain: "one-piece-b5d33.firebaseapp.com",
-    projectId: "one-piece-b5d33",
-    storageBucket: "one-piece-b5d33.firebasestorage.app",
-    messagingSenderId: "483888352023",
-    appId: "1:483888352023:web:efdb4f08244fa65d354552",
-    measurementId: "G-EB0T6687H2"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
